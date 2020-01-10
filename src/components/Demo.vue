f52144c2c618ff120d94bbd87cefa90ce45af324
DULCE <hasin0525@gmail.com>
2020-01-10 10:33:55 +0900

    second commit

---
+++ src/components/Demo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="root">
+    <p>{{originalPrice}}</p>
+    <p>{{doublePrice}}</p>
+    <p>{{triplePrice}}</p>
+  </div>
+</template>
+
+<script>
+import { mapGettes } from "vuex";
+
+export default {
+  computed: {
+    ...mapGettes(["originalPrice", "doublePrice", "triplePrice"])
+  }
+};
+</script>
+
+<style>
+</style>
